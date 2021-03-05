b77aaa984bcb8c7910d55e866d5c3ca30bc73fd0
rakesh.kumar@engineer.ai <rakesh.kumar@engineer.ai>
2021-03-05 10:23:20 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import './App.css';
 import {HomePage} from './pages/homepage/homepage.component';
+import {Switch, Route, Link} from 'react-router-dom';
+
+// const HomePage = (props) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <button onClick={() => props.history.push('/topiclist')}>Topics</button>
+//       <h1>HomePage</h1>
+//     </div>
+//   )
+// }
+
+// const TopicList = (props) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <h1>Topic List</h1>
+//     </div>
+//   )
+// }
+
+// const TopicDetails = (props) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <h1>Topic Details</h1>
+//     </div>
+//   )
+// }
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        {/* <Route exact path='/topiclist' component={TopicList}></Route>
+        <Route path='/topiclist/:id' component={TopicDetails}></Route> */}
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import {withRouter} from 'react-router-dom';
 
-export const MenuItem = ({ title, imageUrl, size}) => (
-        <div className={`${size} menu-item`}>
-            <div className='background-image'
-            style= {{backgroundImage: `url(${imageUrl})`}} />
+export const MenuItem = ({ title, imageUrl, size, history, linkUrl, match}) => (
+    
+        <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+            <div className='background-image' style= {{backgroundImage: `url(${imageUrl})`}} ></div>
             <div className='content'>
                 <h1 className='title'>{title.toUpperCase()}</h1>
                 <span className='subtitle'>SHOP NOW</span>
             </div>
-                
         </div>
+       
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
